3fb8bfdf0c3d720a3ccbcef2fc95fff5e264accc
Victorique Pang <vpang@wolfram.com>
2020-02-07 15:08:01 -0600

    adding changes and cloud build file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HelloWorld';
+
+  list;
+
+  constructor(private data: DataService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.list = this.data.fetchList();
+  }
 }
