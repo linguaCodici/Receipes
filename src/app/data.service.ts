617a501b8c9654af8b85ec042e7246f13779138f
Victorique Pang <vpang@wolfram.com>
2020-02-10 12:08:47 -0600

    updated recipe description

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,7 @@ export class DataService {
 
   private list = [ {
     name: 'Victorique',
-    desc: 'Amazing Chocolate Cake Recipe',
+    desc: 'Amazing Chocolate Mousse Cake Recipe',
     link: 'https://www.bonappetit.com/test-kitchen/how-to/article/dreamy-chocolate-mousse'
   }]
 
